1bce17e1422b18fc5a05fcdd2923670e0b8236e0
diegorezm <dmourarezende@gmail.com>
2024-03-06 16:53:54 -0300

    feat: added products dummy data

--- src/main/java/com/conveniencestore/conveniencestore/components/DummyProductEntitiesData.java
+++ src/main/java/com/conveniencestore/conveniencestore/components/DummyProductEntitiesData.java
@@ -1,7 +1,10 @@
 package com.conveniencestore.conveniencestore.components;
 
+import com.conveniencestore.conveniencestore.domain.ProductEntity.ProductEntity;
 import com.conveniencestore.conveniencestore.domain.ProductEntity.ProductEntityDTO;
+import com.conveniencestore.conveniencestore.domain.Products.ProductDTO;
 import com.conveniencestore.conveniencestore.services.ProductEntityService;
+import com.conveniencestore.conveniencestore.services.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -13,6 +16,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class DummyProductEntitiesData implements CommandLineRunner {
     private final ProductEntityService service;
+    private final ProductService productService;
     private final String[] productNames = {"Milk", "Bread", "Eggs", "Apples", "Bananas", "Orange Juice",
             "Coffee", "Tea", "Cereal", "Pasta", "Tomato Sauce", "Cheese",
             "Yogurt", "Water", "Soda", "Chips", "Candy", "Cookies",
@@ -21,11 +25,13 @@ public class DummyProductEntitiesData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        for (String productName: this.productNames){
+        for (String productName : this.productNames) {
             this.productList.add(new ProductEntityDTO(productName));
         }
-        for (ProductEntityDTO record : productList){
-            this.service.insert(record);
+        for (ProductEntityDTO product : productList) {
+            ProductEntity productEntity = this.service.insert(product);
+            this.productService.insert(new ProductDTO(productEntity.getId()));
+            this.productService.insert(new ProductDTO(productEntity.getId()));
         }
     }
 }
