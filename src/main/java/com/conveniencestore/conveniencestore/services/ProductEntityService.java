7090a5d153942efd792e276d9d91cfa907e32593
diegorezm <dmourarezende@gmail.com>
2024-03-06 16:26:40 -0300

    feat: added search params

--- src/main/java/com/conveniencestore/conveniencestore/services/ProductEntityService.java
+++ src/main/java/com/conveniencestore/conveniencestore/services/ProductEntityService.java
@@ -2,6 +2,7 @@ package com.conveniencestore.conveniencestore.services;
 
 import com.conveniencestore.conveniencestore.domain.ProductEntity.ProductEntity;
 import com.conveniencestore.conveniencestore.domain.ProductEntity.ProductEntityDTO;
+import com.conveniencestore.conveniencestore.domain.ProductEntity.exceptions.ProductEntityNotFoundException;
 import com.conveniencestore.conveniencestore.repositories.ProductEntityRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Sort;
@@ -12,7 +13,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class ProductEntityService {
+public class ProductEntityService  implements ServiceInterface<ProductEntity, ProductEntityDTO>{
     private final ProductEntityRepository productEntityRepository;
 
     public ProductEntity insert(ProductEntityDTO request) {
@@ -22,24 +23,36 @@ public class ProductEntityService {
     }
 
     public ProductEntity delete(int id) {
-        ProductEntity productEntity = this.productEntityRepository.findById(id).orElseThrow();
+        ProductEntity productEntity = this.productEntityRepository.findById(id).orElseThrow(ProductEntityNotFoundException::new);
         this.productEntityRepository.delete(productEntity);
         return productEntity;
     }
 
     public ProductEntity update(int id, ProductEntityDTO productEntityRecord) {
-        ProductEntity productEntity = this.productEntityRepository.findById(id).orElseThrow();
+        ProductEntity productEntity = this.productEntityRepository.findById(id).orElseThrow(ProductEntityNotFoundException::new);
         productEntity.setName(productEntityRecord.name());
         productEntity.setUpdatedAt(LocalDateTime.now());
         this.productEntityRepository.save(productEntity);
         return productEntity;
     }
 
-    public List<ProductEntity> getAll(String orderby) {
-        return productEntityRepository.findAll(Sort.by(Sort.DEFAULT_DIRECTION, orderby));
+    public List<ProductEntity> getAll(String orderby, String order) {
+        Sort.Direction direction;
+        switch (order){
+            case "asc" -> {
+                direction = Sort.Direction.ASC;
+            }
+            case "desc" -> {
+                direction = Sort.Direction.DESC;
+            }
+            default -> {
+                direction = Sort.DEFAULT_DIRECTION;
+            }
+        }
+        return productEntityRepository.findAll(Sort.by(direction, orderby));
     }
 
     public ProductEntity getById(int id) {
-        return this.productEntityRepository.findById(id).orElseThrow();
+        return this.productEntityRepository.findById(id).orElseThrow(ProductEntityNotFoundException::new);
     }
 }
