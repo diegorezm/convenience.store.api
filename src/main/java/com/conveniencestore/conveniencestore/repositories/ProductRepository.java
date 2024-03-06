d422f81caaa9c869ca5992c20c226128f33b32a4
diegorezm <dmourarezende@gmail.com>
2024-03-06 16:26:57 -0300

    added custom queries for products

--- src/main/java/com/conveniencestore/conveniencestore/repositories/ProductRepository.java
+++ src/main/java/com/conveniencestore/conveniencestore/repositories/ProductRepository.java
@@ -2,6 +2,13 @@ package com.conveniencestore.conveniencestore.repositories;
 
 import com.conveniencestore.conveniencestore.domain.Products.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+    @Query("SELECT p FROM Products p WHERE p.sold=FALSE")
+    List<Product> getAllSoldProducts();
+    @Query("SELECT p FROM Products p WHERE p.sold=TRUE")
+    List<Product> getAllNotSoldProducts();
 }
