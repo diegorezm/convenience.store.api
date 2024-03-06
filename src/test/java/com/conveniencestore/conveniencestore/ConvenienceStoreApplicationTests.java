c859098f6f18ce82bba123f7a5bc83530d9cee83
diegorezm <dmourarezende@gmail.com>
2024-03-06 10:01:53 -0300

    initial spring boot commit

---
+++ src/test/java/com/conveniencestore/conveniencestore/ConvenienceStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conveniencestore.conveniencestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvenienceStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
