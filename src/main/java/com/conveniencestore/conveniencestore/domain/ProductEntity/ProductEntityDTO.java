9d2c97798528585a92146863f45c2ccb19aa4b00
diegorezm <dmourarezende@gmail.com>
2024-03-06 16:28:01 -0300

    fix: fixed message

--- src/main/java/com/conveniencestore/conveniencestore/domain/ProductEntity/ProductEntityDTO.java
+++ src/main/java/com/conveniencestore/conveniencestore/domain/ProductEntity/ProductEntityDTO.java
@@ -3,7 +3,7 @@ package com.conveniencestore.conveniencestore.domain.ProductEntity;
 import jakarta.validation.constraints.NotNull;
 
 public record ProductEntityDTO(
-        @NotNull(message = "Name cannot be null")
+        @NotNull(message = "Please provide the name.")
         String name
 ) {
 }
