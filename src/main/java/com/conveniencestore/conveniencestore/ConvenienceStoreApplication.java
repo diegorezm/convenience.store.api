47859af6b4e1712b4dc383b7e8d94bde78f5e6e2
diegorezm <dmourarezende@gmail.com>
2024-03-06 10:30:41 -0300

    main app

--- src/main/java/com/conveniencestore/conveniencestore/ConvenienceStoreApplication.java
+++ src/main/java/com/conveniencestore/conveniencestore/ConvenienceStoreApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ConvenienceStoreApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ConvenienceStoreApplication.class, args);
 	}
