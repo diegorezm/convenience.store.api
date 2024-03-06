f1be0d59cac6f307a0457995102f4d284e7796fd
diegorezm <dmourarezende@gmail.com>
2024-03-06 13:34:51 -0300

    feat: added delete + update

--- src/main/java/com/conveniencestore/conveniencestore/controllers/ProductEntityController.java
+++ src/main/java/com/conveniencestore/conveniencestore/controllers/ProductEntityController.java
@@ -4,26 +4,51 @@ import com.conveniencestore.conveniencestore.domain.models.ProductEntity;
 import com.conveniencestore.conveniencestore.domain.records.ProductEntityRecord;
 import com.conveniencestore.conveniencestore.services.ProductEntityService;
 import jakarta.validation.Valid;
+import jakarta.validation.ValidationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("products/entities")
 public class ProductEntityController {
     private final ProductEntityService service;
+
     @GetMapping
-    public ResponseEntity<?> getAllProducts(){
-        List<ProductEntity> products = this.service.getAll();
+    public ResponseEntity<?> getAllProducts(@RequestParam(required = false) String orderby) {
+        String sortField = Optional.ofNullable(orderby).orElse("id");
+        List<ProductEntity> products = service.getAll(sortField);
         return ResponseEntity.ok().body(products);
     }
 
+    @GetMapping("{id}")
+    public ResponseEntity<?> getProductById(@PathVariable Integer id) {
+        if (id == null) return ResponseEntity.badRequest().build();
+        ProductEntity productEntity = this.service.getById(id);
+        return ResponseEntity.ok().body(productEntity);
+    }
+
     @PostMapping
-    public ResponseEntity<?> registerNewProduct(@RequestBody @Valid ProductEntityRecord request){
-      ProductEntity productEntity = this.service.insert(request);
-      return ResponseEntity.status(201).body(productEntity);
+    public ResponseEntity<?> registerNewProduct(@RequestBody @Valid ProductEntityRecord request) {
+        ProductEntity productEntity = this.service.insert(request);
+        return ResponseEntity.status(201).body(productEntity);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<?> updateProduct(@PathVariable Integer id, @RequestBody @Valid ProductEntityRecord productEntityRecord) {
+        if (id == null) return ResponseEntity.badRequest().build();
+        ProductEntity productEntity = this.service.update(id, productEntityRecord);
+        return ResponseEntity.ok().body(productEntity);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> deleteProduct(@PathVariable Integer id) {
+        if (id == null) return ResponseEntity.badRequest().build();
+        ProductEntity productEntity = this.service.delete(id);
+        return ResponseEntity.ok().body(productEntity);
     }
 }
--- src/main/java/com/conveniencestore/conveniencestore/services/ProductEntityService.java
+++ src/main/java/com/conveniencestore/conveniencestore/services/ProductEntityService.java
@@ -4,8 +4,10 @@ import com.conveniencestore.conveniencestore.domain.models.ProductEntity;
 import com.conveniencestore.conveniencestore.domain.records.ProductEntityRecord;
 import com.conveniencestore.conveniencestore.repositories.ProductEntityRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -19,13 +21,25 @@ public class ProductEntityService {
         return productEntity;
     }
 
-    //    public ProductEntity delete(){
-//    }
-//    public ProductEntity update(){
-//    }
-    public List<ProductEntity> getAll() {
-        return productEntityRepository.findAll();
+    public ProductEntity delete(int id) {
+        ProductEntity productEntity = this.productEntityRepository.findById(id).orElseThrow();
+        this.productEntityRepository.delete(productEntity);
+        return productEntity;
     }
-//    public ProductEntity getById(int id){
-//    }
+
+    public ProductEntity update(int id, ProductEntityRecord productEntityRecord) {
+        ProductEntity productEntity = this.productEntityRepository.findById(id).orElseThrow();
+        productEntity.setName(productEntityRecord.name());
+        productEntity.setUpdatedAt(LocalDateTime.now());
+        this.productEntityRepository.save(productEntity);
+        return productEntity;
+    }
+
+    public List<ProductEntity> getAll(String orderby) {
+        return productEntityRepository.findAll(Sort.by(Sort.DEFAULT_DIRECTION, orderby));
+    }
+
+    public ProductEntity getById(int id) {
+        return this.productEntityRepository.findById(id).orElseThrow();
+    }
 }
