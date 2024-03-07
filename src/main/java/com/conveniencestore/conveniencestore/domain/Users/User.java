22ff471536d60422ac625a6765e711a1e0eca457
diegorezm <dmourarezende@gmail.com>
2024-03-07 00:32:16 -0300

    feat: user model

---
+++ src/main/java/com/conveniencestore/conveniencestore/domain/Users/User.java
@@ -0,0 +1,39 @@
+package com.conveniencestore.conveniencestore.domain.Users;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Users")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String username;
+    private String email;
+    private String password;
+    private String role;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime updatedAt;
+
+    public User(UserDTO data){
+        this.username = data.username();
+        this.email = data.email();
+        this.password = data.password();
+        this.role = data.role();
+    }
+}
