2fca72293383b8adbfed6e35f1bd7212a29768b4
diegorezm <dmourarezende@gmail.com>
2024-03-07 00:32:02 -0300

    feat: added custom jpa functions

--- src/main/java/com/conveniencestore/conveniencestore/repositories/ProductRepository.java
+++ src/main/java/com/conveniencestore/conveniencestore/repositories/ProductRepository.java
@@ -1,15 +1,14 @@
 package com.conveniencestore.conveniencestore.repositories;
 
 import com.conveniencestore.conveniencestore.domain.Products.Product;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
-    @Query("SELECT p FROM Products p WHERE p.sold=TRUE")
-    List<Product> getAllSoldProducts();
-
-    @Query("SELECT p FROM Products p WHERE p.sold=FALSE")
-    List<Product> getAllNotSoldProducts();
+    List<Product> findBySoldTrue(Sort sort);
+    List<Product> findBySoldFalse(Sort sort);
 }
