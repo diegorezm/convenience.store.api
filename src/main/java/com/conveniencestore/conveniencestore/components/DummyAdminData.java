8ad2e2aff4af37060076bb5163b0c93863df9097
diegorezm <dmourarezende@gmail.com>
2024-03-17 00:01:00 -0300

    feat: added dummy admin user

---
+++ src/main/java/com/conveniencestore/conveniencestore/components/DummyAdminData.java
@@ -0,0 +1,26 @@
+package com.conveniencestore.conveniencestore.components;
+
+import com.conveniencestore.conveniencestore.domain.users.User;
+import com.conveniencestore.conveniencestore.domain.users.UserDTO;
+import com.conveniencestore.conveniencestore.domain.users.UserRoles;
+import com.conveniencestore.conveniencestore.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DummyAdminData implements CommandLineRunner {
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public void run(String... args) throws Exception {
+        User user = new User();
+        String password = new BCryptPasswordEncoder().encode("1234");
+        user.setEmail("diego@email.com");
+        user.setUsername("diego");
+        user.setRole(UserRoles.ADMIN);
+        user.setPassword(password);
+        this.userRepository.save(user);
+    }
+}
