8bfb7aa9ed3fa223f07028b55fde841589b49eae
diegorezm <dmourarezende@gmail.com>
2024-03-16 23:57:42 -0300

    fix: added autowired

--- src/main/java/com/conveniencestore/conveniencestore/infra/security/SecurityConfiguration.java
+++ src/main/java/com/conveniencestore/conveniencestore/infra/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.conveniencestore.conveniencestore.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -17,6 +18,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+    @Autowired
     SecurityFilter securityFilter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
--- src/main/java/com/conveniencestore/conveniencestore/infra/security/SecurityFilter.java
+++ src/main/java/com/conveniencestore/conveniencestore/infra/security/SecurityFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,9 +18,10 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @Component
-@RequiredArgsConstructor
 public class SecurityFilter extends OncePerRequestFilter {
+    @Autowired
     TokenService tokenService;
+    @Autowired
     UserRepository userRepository;
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -34,7 +36,7 @@ public class SecurityFilter extends OncePerRequestFilter {
     }
     private String recoverToken(HttpServletRequest request){
         var header = request.getHeader("Authorization");
-        if(header.isEmpty()) return null;
+        if(header == null) return null;
         return header.replace("Bearer ", "");
     }
 }
