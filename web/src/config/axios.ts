8bcae0300864f9da4ee38f7e757e32602ac38167
diegorezm <dmourarezende@gmail.com>
2024-03-20 09:46:53 -0300

    fix: axios auth headers

--- web/src/config/axios.ts
+++ web/src/config/axios.ts
@@ -1,5 +1,20 @@
+import { getToken } from "@/lib/tokenCookieManager"
 import axios from "axios"
 
 export const ax = axios.create({
-  baseURL: 'http://localhost:8080'
+  baseURL: 'http://localhost:8080',
 })
+
+// this should be a temporary solution
+ax.interceptors.request.use(async (config) => {
+  const token = await getToken()
+  config.headers.Authorization = `Bearer ${token}`
+  return config
+}, (error) => {
+  return Promise.reject(error)
+})
+
+export const setAxiosAuthHeader = async (token: string) => {
+    ax.defaults.headers.common.Authorization = `Bearer ${token}`
+    console.log("header: " + ax.defaults.headers.Authorization)
+}
