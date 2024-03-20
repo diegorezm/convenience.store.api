9946a64811f6ecce843e16e11016bdb254e02a6f
diegorezm <dmourarezende@gmail.com>
2024-03-20 09:50:08 -0300

    fix: fixing login url

--- src/main/java/com/conveniencestore/conveniencestore/infra/security/SecurityConfiguration.java
+++ src/main/java/com/conveniencestore/conveniencestore/infra/security/SecurityConfiguration.java
@@ -26,7 +26,7 @@ public class SecurityConfiguration {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/users/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/users").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.PUT, "/users").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.DELETE, "/users").hasRole("ADMIN")
