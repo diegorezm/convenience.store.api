c1f372e712680b55c76c7570f71c9e439970e5f7
diegorezm <dmourarezende@gmail.com>
2024-03-20 09:47:04 -0300

    feat: navbar for login and logout

--- web/src/components/navbar/index.tsx
+++ web/src/components/navbar/index.tsx
@@ -1,46 +1,86 @@
 "use client"
 
+import { useAuthStore } from "@/lib/useAuthStore"
 import ModeToggle from "./modeToggle"
-import { LucideIcon, HomeIcon, ScanBarcode, ArrowLeftRight } from 'lucide-react'
-import Link from "next/link"
+import { LucideIcon, HomeIcon, ScanBarcode, ArrowLeftRight, LogIn, LogOutIcon } from 'lucide-react'
+import Button from "next/link"
 import { usePathname } from "next/navigation"
+import { useEffect, useState } from "react"
+import { logout } from "@/app/actions/userActions"
+import toast from "react-hot-toast"
+
+type Tag = {
+  name: string
+  Icon: LucideIcon
+  path: string
+}
+const noAuth: Tag[] = [
+  {
+    name: "home",
+    Icon: HomeIcon,
+    path: "/"
+  },
+  {
+    name: "login",
+    Icon: LogIn,
+    path: "/login"
+  }
+]
+const tags: Tag[] = [
+  {
+    name: "home",
+    Icon: HomeIcon,
+    path: "/"
+
+  },
+  {
+    name: "products",
+    Icon: ScanBarcode,
+    path: "/products"
+
+  },
+  {
+    name: "transaction",
+    Icon: ArrowLeftRight,
+    path: "/transactions"
+  }
+]
+
 
 export default function Navbar() {
-  type Tag = {
-    name: string
-    Icon: LucideIcon
-    path: string
-  }
-  const tags: Tag[] = [
-    {
-      name: "home",
-      Icon: HomeIcon,
-      path: "/"
-
-    },
-    {
-      name: "products",
-      Icon: ScanBarcode,
-      path: "/products"
-
-    },
-    {
-      name: "transaction",
-      Icon: ArrowLeftRight,
-      path: "/transactions"
-    }
-  ]
+  const { userRemove } = useAuthStore()
+  const { authorized } = useAuthStore()
+  const [links, setLinks] = useState<Tag[]>([])
   const pathName = usePathname()
+
+  const handleLogout = async () => {
+    userRemove();
+    await logout();
+    toast.success("Logged out!")
+  }
+
+  useEffect(() => {
+    authorized ? setLinks(tags) : setLinks(noAuth)
+  }, [authorized])
   return (
     <nav className="flex justify-end p-1 items-center space-x-3">
       <ul className="flex flex-row p-2 gap-5">
-        {tags.map((e, i) => (
+        {links.map((e, i) => (
           <li key={i}>
-            <Link href={e.path} className={`flex flex-row justify-center items-center text-center  gap-1 text-lg ${pathName === e.path ? "border-b-2 border-b-primary" : ""}`}>
+            <Button href={e.path} className={`flex flex-row justify-center items-center text-center  gap-1 text-lg ${pathName === e.path ? "border-b-2 border-b-primary" : ""}`}>
               <e.Icon className="w-5 h-5" /> {e.name}
-            </Link>
+            </Button>
           </li>
         )
+        )}
+        {authorized && (
+          <li>
+            <Button href={"/login"} className="flex flex-row justify-center items-center text-center  gap-1 text-lg" onClick={handleLogout}>
+              <LogOutIcon className="w-5 h-5" />
+              Logout
+            </Button>
+
+          </li>
         )}
       </ul>
       <ModeToggle />
