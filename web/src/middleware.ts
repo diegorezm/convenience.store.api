dbbd85c72daa8ec2588bbf8169b898e9e230331f
diegorezm <dmourarezende@gmail.com>
2024-03-22 23:58:36 -0300

    fix: now cant login twice

--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -3,10 +3,13 @@ import { type NextRequest, NextResponse } from "next/server";
 export function middleware(request: NextRequest) {
   const cookies = request.cookies
   const auth = cookies.get("auth")?.value
-  if(auth && auth == "true"){
-      return NextResponse.next()
+  if (auth && auth == "true") {
+    if (request.nextUrl.pathname.startsWith("/login")) {
+      return NextResponse.redirect(new URL('/', request.url));
+    }
+    return NextResponse.next()
   }
- return NextResponse.redirect(new URL('/login', request.url))
+  return NextResponse.redirect(new URL('/login', request.url))
 }
 export const config = {
   matcher: ["/products", "/transactions"]
