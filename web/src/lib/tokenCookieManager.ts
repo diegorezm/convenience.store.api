73a8079105a971d2e2d215447b2d39ea866f4f1a
diegorezm <dmourarezende@gmail.com>
2024-03-22 23:58:25 -0300

    fix: fixing bug with cookiestore const
    i have to turn this into a class

--- web/src/lib/tokenCookieManager.ts
+++ web/src/lib/tokenCookieManager.ts
@@ -1,16 +1,17 @@
 "use server"
 import { cookies } from 'next/headers'
 
-const cookieStore = cookies()
-
 export async function setToken(token: string) {
+  const cookieStore = cookies()
   cookieStore.set("token", token)
 }
 export async function getToken() {
+  const cookieStore = cookies()
   const token = cookieStore.get("token")
   return token?.value ?? ""
 }
 
 export async function deleteToken() {
+  const cookieStore = cookies()
   cookieStore.set("token", "")
 }
