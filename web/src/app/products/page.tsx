dd86b93392a986e60225a26dfe6f347e63bb5799
diegorezm <dmourarezende@gmail.com>
2024-03-23 20:21:49 -0300

    feat: parsing query params

--- web/src/app/products/page.tsx
+++ web/src/app/products/page.tsx
@@ -2,19 +2,22 @@
 import { getAllProductEntities } from "../actions/productEntityActions"
 import { DataTable } from "@/components/datatable"
 import { entityColumns } from "../columns/entityCols"
-import { useEffect, useState } from "react"
+import { Dispatch, SetStateAction, useEffect, useState } from "react"
 import ProductEntity from "../models/productEntity"
 import toast from "react-hot-toast"
 import ProductsInputFilter from "./ProductsInputFilter"
 import { usePathname, useRouter, useSearchParams } from "next/navigation"
 import { EntityActionsParam } from "../queryParams/productEntityQueryParams"
-import EditDropdown from "./EditDropdown"
-import InfoDropdown from "./InfoDropdown"
-import DeleteDropdown from "./DeleteDropdown"
+import EditEntityDropdown from "./EditDropdown"
+import EntityInfoDropdown from "./InfoDropdown"
+import DeleteEntityDropdown from "./DeleteEntityDropdown"
+import { ProductActionsParam } from "../queryParams/productsQueryParams"
+import DeleteProductDropdown from "./DeleteProductDropdown"
 
 export type EntityDropdownProps = {
   clearParams: () => void
   id: string
+  setProductEntities: Dispatch<SetStateAction<ProductEntity[]>>
 }
 
 export default function Products() {
@@ -31,9 +34,11 @@ export default function Products() {
     const showInfoParam = params.get(EntityActionsParam.showInfo) ?? ""
     const deleteEntityParam = params.get(EntityActionsParam.deleteEntity) ?? ""
     const editEntityParam = params.get(EntityActionsParam.editEntity) ?? ""
-    if (params.has(EntityActionsParam.showInfo)) return <InfoDropdown clearParams={clearParams} id={showInfoParam} />
-    if (params.has(EntityActionsParam.deleteEntity)) return <DeleteDropdown clearParams={clearParams} id={deleteEntityParam} />
-    if (params.has(EntityActionsParam.editEntity)) return <EditDropdown clearParams={clearParams} id={editEntityParam} />
+    const deleteProductParam = params.get(ProductActionsParam.deleteProduct) ?? ""
+    if (params.has(EntityActionsParam.showInfo)) return <EntityInfoDropdown clearParams={clearParams} id={showInfoParam} setProductEntities={setProductEntities}/>
+    if (params.has(EntityActionsParam.deleteEntity)) return <DeleteEntityDropdown clearParams={clearParams} id={deleteEntityParam} setProductEntities={setProductEntities}/>
+    if (params.has(EntityActionsParam.editEntity)) return <EditEntityDropdown clearParams={clearParams} id={editEntityParam} setProductEntities={setProductEntities}/>
+      if(params.has(ProductActionsParam.deleteProduct)) return <DeleteProductDropdown clearParams={clearParams} id={deleteProductParam}/>
     return null
   }
 
